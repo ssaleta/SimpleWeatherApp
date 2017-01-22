a3dbb4d02d30445db378f464da5ab4d754d418e7
ssaleta <sebastiansaleta@gmail.com>
2017-01-22 21:12:45 +0100

    add icon

--- app/src/main/java/com/example/sebastian/simpleweatherapp/MainActivity.java
+++ app/src/main/java/com/example/sebastian/simpleweatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sebastian.simpleweatherapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -16,6 +17,8 @@ import com.android.volley.VolleyError;
 import com.example.sebastian.simpleweatherapp.Model.Location;
 import com.example.sebastian.simpleweatherapp.Model.Weather;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
 import java.text.DecimalFormat;
 
 import butterknife.BindView;
@@ -59,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         httpRequestHandler = HTTPRequestHandler.getInstance();
         httpRequestHandler.init(getApplicationContext());
         httpRequestHandler.sendGetRequest(BASE_URL + city + OPEN_WEATHER_KEY, getResponseListener(), getErrorListener());
+
         setLocation.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
@@ -104,10 +108,14 @@ public class MainActivity extends AppCompatActivity {
         Weather weather = gson.fromJson(json, Weather.class);
         Location location = gson.fromJson(json, Location.class);
         initializeView(weather, location);
+
     }
 
     public void initializeView(Weather weather, Location location) {
 
+        Picasso.with(this)
+                .load(IMG_URL + weather.weatherDescription.get(0).getIcon() + ".png")
+                .into(weatherImg);
         DecimalFormat oneDecimalPlace = new DecimalFormat("##.#");
         cityname.setText(location.getCity() + "," + location.country.getCountry());
         condition.setText(weather.weatherDescription.get(0).getDescription());
@@ -115,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
         humidity.setText(Float.toString(weather.currentCondition.getHumidity()) + " %");
         pressure.setText(Float.toString(weather.currentCondition.getPressure()) + " hPa");
         temperature.setText(oneDecimalPlace.format(weather.currentCondition.getTemperature() - 272.15) + " °C");
-
     }
 }
 
